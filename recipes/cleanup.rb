df33bcd49a478735ca7c296e8480393215bfe990
Giovanni Intini <intinig@gmail.com>
2015-01-13 15:05:51 +0100

    fixed typo in cleanup

--- recipes/cleanup.rb
+++ recipes/cleanup.rb
@@ -1,4 +1,4 @@
 execute "cleaning up all unused images" do
-  Chef::Log.info("Cleaning up unused images..."
+  Chef::Log.info("Cleaning up unused images...")
   command "docker rmi `docker images -q -f dangling=true`"
 end

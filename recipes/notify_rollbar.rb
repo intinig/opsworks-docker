5d3faac0bea5e27856ef3dc4e8c9044144825902
Giovanni Intini <intinig@gmail.com>
2015-01-27 10:45:40 +0100

    moving the notifications block outside execute

--- recipes/notify_rollbar.rb
+++ recipes/notify_rollbar.rb
@@ -6,13 +6,13 @@ node["deploy"].each do |application, deploy|
 
   deploy["containers"].each do |c|
     c.each do |app_name, app_config|
-      execute "notifying rollbar of #{app_name} deployment" do
-        access_token = app_config["notifications"]["rollbar"]["access_token"]
-        env_var = app_config["notifications"]["rollbar"]["env_var"]
-        rev_var = app_config["notifications"]["rollbar"]["rev_var"]
-        command "docker exec #{app_name}0 -c 'curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{access_token} -F environment=$#{env_var} -F revision=#{rev_var} -F local_username=#{deploy["user"]}'"
-
-        only_if { app_config["notifications"] && app_config["notifications"]["rollbar"] }
+      if app_config["notifications"] && app_config["notifications"]["rollbar"]
+        execute "notifying rollbar of #{app_name} deployment" do
+          access_token = app_config["notifications"]["rollbar"]["access_token"]
+          env_var = app_config["notifications"]["rollbar"]["env_var"]
+          rev_var = app_config["notifications"]["rollbar"]["rev_var"]
+          command "docker exec #{app_name}0 -c 'curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{access_token} -F environment=$#{env_var} -F revision=#{rev_var} -F local_username=#{deploy["user"]}'"
+        end
       end
     end
   end

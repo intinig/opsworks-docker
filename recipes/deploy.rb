d87c81d9cdf4d9f91232c03c4d0266e787b63137
Giovanni Intini <intinig@gmail.com>
2015-01-29 13:01:03 +0100

    big changes to deploy recipe

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -27,33 +27,34 @@ node["deploy"].each do |application, deploy|
           block do
             sleep(app_config["startup_time"].to_i) if app_config["startup_time"] && i > 0
           end
+          not_if { e.manual? }
         end
 
-        unless e.manual?
-          execute "kill running #{app_name}#{i} container" do
-            Chef::Log.info("Killing running #{application}/#{app_name}#{i} container...")
-            command "docker kill #{app_name}#{i}"
-            only_if "docker ps -f status=running | grep ' #{app_name}#{i} '"
-          end
+        execute "kill running #{app_name}#{i} container" do
+          Chef::Log.info("Killing running #{application}/#{app_name}#{i} container...")
+          command "docker kill #{app_name}#{i}"
+          only_if { system("docker ps -f status=running | grep ' #{app_name}#{i} '") && e.auto? }
+        end
 
-          execute "remove stopped #{app_name}#{i} container" do
-            Chef::Log.info("Removing the #{application}/#{app_name}#{i} container...")
-            command "docker rm  #{app_name}#{i}"
-            only_if "docker ps -a | grep ' #{app_name}#{i} '"
-          end
-          execute "migrate #{app_name}#{i} container" do
-            Chef::Log.info("Migrating #{app_name}#{i}...")
-            command "docker run --rm #{e.env_string(environment)} #{e.links} #{e.volumes} #{e.volumes_from} #{image} #{app_config["migration"]}"
-            only_if { e.migrate? && i == 0}
-          end
+        execute "remove stopped #{app_name}#{i} container" do
+          Chef::Log.info("Removing the #{application}/#{app_name}#{i} container...")
+          command "docker rm  #{app_name}#{i}"
+          only_if "docker ps -af status=exited | grep ' #{app_name}#{i} '"
+        end
+
+        execute "migrate #{app_name}#{i} container" do
+          Chef::Log.info("Migrating #{app_name}#{i}...")
+          command "docker run --rm #{e.env_string(environment)} #{e.links} #{e.volumes} #{e.volumes_from} #{image} #{app_config["migration"]}"
+          only_if { e.migrate? && i == 0 && e.auto? }
         end
 
         execute "launch #{app_name}#{i} container" do
           (history = Mixlib::ShellOut.new "docker history -q #{image} | head -1").run_command
           environment["RELEASE_TAG"] = history.stdout.strip
+
           Chef::Log.info("Launching #{image}...")
           command "docker run -d -h #{e.hostname i} --name #{app_name}#{i} #{e.ports} #{e.env_string(environment)} #{e.links} #{e.volumes} #{e.volumes_from} #{e.entrypoint} #{image} #{e.cmd i}"
-          not_if "docker ps -a | grep ' #{app_name}#{i} '"
+          not_if {"docker ps -f status=running | grep ' #{app_name}#{i} '"}
         end
 
         cron "#{app_name}#{i} cron" do
@@ -62,7 +63,7 @@ node["deploy"].each do |application, deploy|
           hour e.cron["hour"]
           weekday e.cron["weekday"]
           command "docker run --rm --name #{app_name}#{i} #{e.env_string(environment)} #{e.links} #{e.volumes} #{e.volumes_from} #{e.entrypoint} #{image} #{app_config["command"]}"
-          only_if { e.cron? }
+          only_if { e.cron? && i==0 }
         end
 
       end
